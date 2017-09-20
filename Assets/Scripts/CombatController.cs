0d79c383f932219b9c45fdd5ac50911fe332228a
MKlok <Martijnklok@hotmail.nl>
2017-09-20 14:51:42 +0200

    Initial commit

---
+++ Assets/Scripts/CombatController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombatController : MonoBehaviour {
+    private GameObject target;
+    public Sprite[] animationHandler;
+    public Slider ccCooldown;
+    public Image sliderFill;
+
+    private float resetTimer;
+    private float turnCooldown;
+
+	// Use this for initialization
+	void Start () {
+        target = null;
+
+        resetTimer = 0.0f;
+        turnCooldown = 2.5f;
+
+        ccCooldown.maxValue = turnCooldown;
+        sliderFill.color = Color.green;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+            if (hit)
+            {
+                EnemyController ec = hit.transform.gameObject.GetComponent<EnemyController>();
+                if (ec)
+                {
+                    target = hit.transform.gameObject;
+                }
+                if (!ec)
+                {
+                    if (ccCooldown.value >= turnCooldown)
+                    {
+                        if (hit.transform.tag == "Attack")
+                        {
+                            if (target != null && target.GetComponent<EnemyController>().HP > 0)
+                            {
+                                target.GetComponent<EnemyController>().HP -= 20;
+
+                                Debug.Log(target.GetComponent<EnemyController>().HP);
+
+                                animationUpdate(1);
+                            }
+                        }
+                        else if (hit.transform.tag == "Defend")
+                        {
+                            animationUpdate(2);
+                        }
+                        else if (hit.transform.tag == "Heal")
+                        {
+                            animationUpdate(3);
+                        }
+                        ccCooldown.value = 0;
+                    }
+                }
+            }
+        }
+        if (resetTimer < 2f)
+        {
+            resetTimer += Time.deltaTime;
+            if (resetTimer >= 2f)
+            {
+                animationUpdate(0);
+            }
+        }
+        if (ccCooldown.value < turnCooldown)
+        {
+            ccCooldown.value += Time.deltaTime;
+            if (sliderFill.color == Color.yellow)
+            {
+                sliderFill.color = Color.green;
+            }
+        }
+        else
+        {
+            sliderFill.color = Color.yellow;
+        }
+
+    }
+
+    void animationUpdate(int frame)
+    {
+        if (frame >= animationHandler.Length)
+        {
+            frame = 0;
+        }
+        if (frame != 0)
+        {
+            resetTimer = 0.0f;
+        }
+        GetComponent<SpriteRenderer>().sprite = animationHandler[frame];
+    }
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+    public int HP;
+    private Color colorIni;
+    private Color colorFin;
+    private float duration;
+    Color lerpedColor;
+
+    private float t;
+    private bool ColorChanged;
+
+    Renderer _renderer;
+
+    // Use this for initialization
+    void Start () {
+        HP = 100;
+
+        colorIni = Color.white;
+        colorFin = Color.black;
+        duration = 3f;
+        lerpedColor = Color.white;
+
+        t = 0;
+        ColorChanged = false;
+
+        _renderer = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (HP <= 0)
+        {
+            //GetComponent<SpriteRenderer>().color = Color.black;
+            //Debug.Log("White");
+
+            lerpedColor = Color.Lerp(colorIni, colorFin, t);
+            _renderer.material.color = lerpedColor;
+
+            t += Time.deltaTime;
+                if (t >= duration)
+                    ColorChanged = true;
+            
+           
+        }
+	}
+}
