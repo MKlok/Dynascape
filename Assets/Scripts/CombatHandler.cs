e1c8faf177dce9ca59b54a66d51e103e0f46409a
MKlok <Martijnklok@hotmail.nl>
2017-10-18 09:32:01 +0200

    Added basic functionality to combat handler

--- Assets/Scripts/CombatHandler.cs
+++ Assets/Scripts/CombatHandler.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CombatHandler : MonoBehaviour {
+    private GameObject[] enemyList;
 
 	// Use this for initialization
 	void Start () {
-		
+        enemyList = GameObject.FindGameObjectsWithTag("enemy");
+
+        Debug.Log(enemyList);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void LoadOverworld ()
+    {
+        SceneManager.LoadScene("OverworldScene");
+    }
 }
