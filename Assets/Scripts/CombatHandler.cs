b410bb6ab7273d3a7b973104beeb8fb918ee750b
MKlok <Martijnklok@hotmail.nl>
2017-10-18 09:38:55 +0200

    Added UpdateList

--- Assets/Scripts/CombatHandler.cs
+++ Assets/Scripts/CombatHandler.cs
@@ -3,23 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CombatHandler : MonoBehaviour {
+public class CombatHandler : MonoBehaviour
+{
     private GameObject[] enemyList;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         enemyList = GameObject.FindGameObjectsWithTag("Enemy");
 
         Debug.Log(enemyList.Length);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
 
-    private void LoadOverworld ()
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void LoadOverworld()
     {
         SceneManager.LoadScene("OverworldScene");
+    }
+
+    public void UpdateList()
+    {
+        enemyList = GameObject.FindGameObjectsWithTag("Enemy");
     }
 }
