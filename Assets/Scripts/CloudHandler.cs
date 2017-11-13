fc9ffcad7f9670604bcbe7a0841a1ad6f65c735c
MKlok <Martijnklok@hotmail.nl>
2017-11-13 14:27:59 +0100

    Added new assets

--- Assets/Scripts/CloudHandler.cs
+++ Assets/Scripts/CloudHandler.cs
@@ -9,12 +9,22 @@ public class CloudHandler : MonoBehaviour {
     private float spawnTimer;
     private float spawnTime;
 
-	// Use this for initialization
-	void Start () {
+
+    public float orthographicSize = 5;
+    public float aspect = 1.33333f;
+
+    // Use this for initialization
+    void Start () {
         spawnTimer = 0.0f;
         spawnTime = Random.Range(0.4f, 1.2f);
 
         InstanciateOcean(61, 55);
+
+
+        Camera.main.projectionMatrix = Matrix4x4.Ortho(
+            -orthographicSize * aspect, orthographicSize * aspect,
+            -orthographicSize, orthographicSize,
+            Camera.main.nearClipPlane, Camera.main.farClipPlane);
     }
 	
 	// Update is called once per frame
