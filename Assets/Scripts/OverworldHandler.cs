24f224d7caba7481d792704a4c1961db80e29f1a
MKlok <Martijnklok@hotmail.nl>
2017-11-13 14:28:31 +0100

    Moved Camera scaling to Cloudhandler

--- Assets/Scripts/OverworldHandler.cs
+++ Assets/Scripts/OverworldHandler.cs
@@ -7,9 +7,6 @@ public class OverworldHandler : MonoBehaviour {
     private PlayerCharacter pc;
     public PlayerInput pin;
 
-    public float orthographicSize = 5;
-    public float aspect = 1.33333f;
-
     public Transform pauzeScreen;
     private Transform pauze;
 
@@ -30,11 +27,6 @@ public class OverworldHandler : MonoBehaviour {
         charStorage = new GameObject[charSplash.Length];
 
         pauze = null;
-
-        Camera.main.projectionMatrix = Matrix4x4.Ortho(
-            -orthographicSize * aspect, orthographicSize * aspect,
-            -orthographicSize, orthographicSize,
-            Camera.main.nearClipPlane, Camera.main.farClipPlane);
     }
 
 	// Update is called once per frame
