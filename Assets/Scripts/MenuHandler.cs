084306b324385fcb8c9d70b03302df56993eee7b
MKlok <Martijnklok@hotmail.nl>
2017-11-13 14:59:34 +0100

    Added scenemanagement to MenuHandler

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuHandler : MonoBehaviour {
     public Text[] uiText;
@@ -15,12 +16,26 @@ public class MenuHandler : MonoBehaviour {
             int i2 = i * 2 - 1;
 
             uiText[i].transform.position = new Vector3(Screen.width / 2 + (Screen.width / 3.5f) * i2, Screen.height / 3.4f);
-            //r = Width / 3.5
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+            if (hit)
+            {
+                if (hit.transform.gameObject.name == "Quit")
+                {
+                    Application.Quit();
+                }
+                else if (hit.transform.gameObject.name == "Play")
+                {
+                    SceneManager.LoadScene("OverworldScene");
+                }
+            }
+        }
+    }
 }
